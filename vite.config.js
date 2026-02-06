f4851f8598dba5127aedc7ab11a9924e2518c0e5
OligarchC <landrycheuko@gmail.com>
2026-02-06 15:04:40 -0500

    Fix base path for kelvin Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  base: '/kelvin/',
+  base: '/Kelvin/',
   plugins: [
     vue(),
     vuetify({ autoImport: true })
